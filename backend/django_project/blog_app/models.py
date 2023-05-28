c11b971f36b22c1a121cbbdcc462e7e5dae3c2a3
Nathan Foss <nathanfoss.dev@gmail.com>
2023-05-27 19:36:14 -0500

    Connect to railway database

--- backend/django_project/blog_app/models.py
+++ backend/django_project/blog_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Role(models.Model):
     type = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='roles')
 
 class User(models.Model):
     first_name = models.CharField(max_length=50)
@@ -11,40 +11,40 @@ class User(models.Model):
     email = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     profile_image = models.TextField()
-    role = models.ForeignKey('Role', on_delete=models.CASCADE)
-    comment = models.ForeignKey('BlogPost', on_delete=models.CASCADE)
+    role = models.ForeignKey('Role', on_delete=models.CASCADE, related_name='users')
+    comment = models.ForeignKey('BlogPost', on_delete=models.CASCADE, related_name='comments')
 
 class Comment(models.Model):
     content = models.CharField(max_length=1000)
     like = models.ForeignKey('CommentLike', on_delete=models.CASCADE)
 
 class CommentLike(models.Model):
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='comment_likes')
 
 class Tag(models.Model):
     name = models.CharField(max_length=50)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
-    blog = models.ForeignKey('BlogPost', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='tags')
+    blog = models.ForeignKey('BlogPost', on_delete=models.CASCADE, related_name='tags')
 
 class BlogPostCategory(models.Model):
     name = models.CharField(max_length=50)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
-    blog = models.ForeignKey('BlogPost', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='categories')
+    blog = models.ForeignKey('BlogPost', on_delete=models.CASCADE, related_name='categories')
 
 class BlogPostLike(models.Model):
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='blog_likes')
 
 class BlogPostFavorite(models.Model):
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
-    blog = models.ForeignKey('BlogPost', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='blog_favorites')
+    blog = models.ForeignKey('BlogPost', on_delete=models.CASCADE, related_name='favorites')
 
 class BlogPost(models.Model):
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='blog_posts')
     title = models.CharField(max_length=50)
     image = models.TextField()
     video = models.TextField()
     content = models.CharField(max_length=5000)
     expected_read_time = models.CharField(max_length=50)
-    comment = models.ForeignKey('Comment', on_delete=models.CASCADE)
-    post_category = models.ForeignKey('BlogPostCategory', on_delete=models.CASCADE)
-    post_tag = models.ForeignKey('Tag', on_delete=models.CASCADE)
+    comment = models.ForeignKey('Comment', on_delete=models.CASCADE, related_name='blog_posts')
+    post_category = models.ForeignKey('BlogPostCategory', on_delete=models.CASCADE, related_name='blog_posts')
+    post_tag = models.ForeignKey('Tag', on_delete=models.CASCADE, related_name='blog_posts')
--- backend/django_project/django_project/settings.py
+++ backend/django_project/django_project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'uo7EpnHftH48SUlkH8Uj',
+        'HOST': 'containers-us-west-148.railway.app',
+        'PORT': '7425',
     }
 }
 
