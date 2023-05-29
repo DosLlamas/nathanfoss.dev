6fa459da900cec64019f35e1c65c6ac1db480ac9
Nathan Foss <nathanfoss.dev@gmail.com>
2023-05-28 20:30:21 -0500

    Successfully fetch from React app to Django backend

--- frontend/react-blog/src/Home.js
+++ frontend/react-blog/src/Home.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
+
+  const [djangoResp, setDjangoResp] = useState("")
+
+  useEffect(()=> {
+    fetch("http://127.0.0.1:8000/blogs/")
+    .then(r => r.json())
+    .then(data => setDjangoResp(data))// setDjangoResp(data)
+  }, [])
+
   return (
     <div>
-      Home
+      <h1>Home</h1>
+      <p>Response from django API: {djangoResp? djangoResp : "No response"}</p>
     </div>
   );
 }
