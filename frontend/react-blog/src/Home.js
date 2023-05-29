84da7598504878db4e98e1501bf3220e1a089a93
Nathan Foss <nathanfoss.dev@gmail.com>
2023-05-28 20:46:00 -0500

    Handle errors on frontend home page

--- frontend/react-blog/src/Home.js
+++ frontend/react-blog/src/Home.js
@@ -1,19 +1,29 @@
 import React, { useState, useEffect } from "react";
 
 export default function Home() {
+  const [djangoResp, setDjangoResp] = useState("");
+  const [errors, setErrors] = useState("");
 
-  const [djangoResp, setDjangoResp] = useState("")
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/blogs/").then((res) => {
+      if (res.ok) {
+        res.json().then((data) => {
+          setDjangoResp(data);
+        });
+      } else {
+        res.json().then((errorData) => setErrors(errorData.errors));
+      }
+    });
+  }, []);
 
-  useEffect(()=> {
-    fetch("http://127.0.0.1:8000/blogs/")
-    .then(r => r.json())
-    .then(data => setDjangoResp(data))// setDjangoResp(data)
-  }, [])
-
-  return (
+  errors ? (
+    <div>
+      <p>{errors}</p>
+    </div>
+  ) : (
     <div>
       <h1>Home</h1>
-      <p>Response from django API: {djangoResp? djangoResp : "No response"}</p>
+      <p>Response from django API: {djangoResp ? djangoResp : "No response"}</p>
     </div>
   );
 }
