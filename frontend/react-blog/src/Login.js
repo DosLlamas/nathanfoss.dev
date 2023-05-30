f18f0be3603c56c38f0cbcad796b54a6516dc865
Nathan Foss <nathanfoss.dev@gmail.com>
2023-05-30 10:23:07 -0500

    Login screen transitions

--- frontend/react-blog/src/Login.js
+++ frontend/react-blog/src/Login.js
@@ -1,8 +1,8 @@
 // Assets
 import Pika from "./assets/pika.gif";
 // Packages
-import React, { useState } from "react";
-import { useNavigate} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import LoginIcon from "@mui/icons-material/Login";
 import PersonIcon from "@mui/icons-material/Person";
 import Box from "@mui/material/Box";
@@ -20,6 +20,11 @@ import InputLabel from "@mui/material/InputLabel";
 import OutlinedInput from "@mui/material/OutlinedInput";
 
 export default function Login() {
+  const [transitionedIn, setTransitionedIn] = useState(false);
+
+  useEffect(() => {
+    setTransitionedIn(true);
+  }, []);
   const navigate = useNavigate();
 
   const [showPassword, setShowPassword] = useState(false);
@@ -30,9 +35,9 @@ export default function Login() {
     event.preventDefault();
   };
 
-  const navigateToSignupPage = (e) =>{
-    navigate('/signup')
-}
+  const navigateToSignupPage = (e) => {
+    navigate("/signup");
+  };
 
   // const [djangoResp, setDjangoResp] = useState("");
   // const [errors, setErrors] = useState(null);
@@ -57,116 +62,118 @@ export default function Login() {
   // if (errors) return <h1>{errors}</h1>;
   return (
     <div className="primary-container">
-      <div className="welcome-card">
-        <div className="light-dark-mode">
-          <NightlightIcon
-            sx={{
+      <div
+        className={`transition-in ${transitionedIn ? "transitioned-in" : ""}`}
+      >
+        <div className="welcome-card">
+          <div className="light-dark-mode">
+            <NightlightIcon
+              sx={{
+                display: "flex",
+                fontSize: "74px",
+                color: "#1f1f1f",
+                cursor: "pointer",
+                transition: "color 0.3s",
+                "&:hover": {
+                  color: "#e7d93c",
+                },
+              }}
+            />
+          </div>
+          <h1 style={{ fontSize: "88px" }}>Welcome</h1>
+          <p style={{ fontSize: "22px" }}>
+            nathanfoss.dev <b style={{ color: "#085786" }}>web</b>
+          </p>
+          <div
+            style={{
               display: "flex",
-              fontSize: "74px",
-              color: "#1f1f1f",
-              cursor: "pointer",
-              transition: "color 0.3s",
-              "&:hover": {
-                color: "#e7d93c", 
-              },
+              alignItems: "center",
+              justifyContent: "center",
+              marginBottom: "-10%",
+              marginTop: "-10%",
             }}
-          />
-        </div>
-        <h1 style={{ fontSize: "88px" }}>
-          Welcome
-          </h1>
-        <p style={{ fontSize: "22px" }}>
-          nathanfoss.dev <b style={{ color: "#085786" }}>web</b>
-        </p>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            marginBottom: "-10%",
-            marginTop: "-10%",
-          }}
-        >
-          <img src={Pika} alt="Pikachu running" width="70%" height="auto" />
-        </div>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <div className="guest-btn">
-            <div className="guest-btn-content">
-              <PersonIcon /> Continue as a guest
+          >
+            <img src={Pika} alt="Pikachu running" width="70%" height="auto" />
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <div className="guest-btn">
+              <div className="guest-btn-content">
+                <PersonIcon /> Continue as a guest
+              </div>
             </div>
           </div>
-        </div>
-        <div className="or-container">
-          <div className="or-left-line"></div>
-          <p className="or">OR</p>
-          <div className="or-right-line"></div>
-        </div>
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, color: "#c4c7c5" },
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
+          <div className="or-container">
+            <div className="or-left-line"></div>
+            <p className="or">OR</p>
+            <div className="or-right-line"></div>
+          </div>
           <Box
-            fullwidth
-            sx={{ display: "flex", alignItems: "flex-end", width: "100%" }}
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1, color: "#c4c7c5" },
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
           >
-            <EmailIcon sx={{ fontSize: "50px" }} />
-            <TextField required fullWidth label="Email" variant="outlined" />
-          </Box>
-          <Box
-            fullwidth
-            sx={{ display: "flex", alignItems: "flex-end", width: "100%" }}
-          >
-            <LockIcon sx={{ fontSize: "50px" }} />
-
             <Box
               fullwidth
               sx={{ display: "flex", alignItems: "flex-end", width: "100%" }}
             >
-              <FormControl fullWidth variant="outlined">
-                <InputLabel
-                  sx={{ borderRadius: "24px" }}
-                  htmlFor="outlined-adornment-password"
-                  required
-                >
-                  Password
-                </InputLabel>
-                <OutlinedInput
-                  id="outlined-adornment-password"
-                  type={showPassword ? "text" : "password"}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        edge="end"
-                      >
-                        {showPassword ? (
-                          <VisibilityIcon />
-                        ) : (
-                          <VisibilityOffIcon />
-                        )}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                  label="Password"
-                />
-              </FormControl>
+              <EmailIcon sx={{ fontSize: "50px" }} />
+              <TextField required fullWidth label="Email" variant="outlined" />
+            </Box>
+            <Box
+              fullwidth
+              sx={{ display: "flex", alignItems: "flex-end", width: "100%" }}
+            >
+              <LockIcon sx={{ fontSize: "50px" }} />
+
+              <Box
+                fullwidth
+                sx={{ display: "flex", alignItems: "flex-end", width: "100%" }}
+              >
+                <FormControl fullWidth variant="outlined">
+                  <InputLabel
+                    sx={{ borderRadius: "24px" }}
+                    htmlFor="outlined-adornment-password"
+                    required
+                  >
+                    Password
+                  </InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? "text" : "password"}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showPassword ? (
+                            <VisibilityIcon />
+                          ) : (
+                            <VisibilityOffIcon />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Password"
+                  />
+                </FormControl>
+              </Box>
             </Box>
           </Box>
-        </Box>
-        <div className="contact-card-wrapper" style={{ cursor: "pointer" }}>
-          <div className="contact-card">
-            <LoginIcon /> Sign in
-          </div>
-          <div className="contact-card" onClick={navigateToSignupPage}>
-            Need an account? <span>Sign up</span>
+          <div className="contact-card-wrapper" style={{ cursor: "pointer" }}>
+            <div className="contact-card">
+              <LoginIcon /> Sign in
+            </div>
+            <div className="contact-card" onClick={navigateToSignupPage}>
+              Need an account? <span>Sign up</span>
+            </div>
           </div>
         </div>
       </div>
--- frontend/react-blog/src/Signup.js
+++ frontend/react-blog/src/Signup.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import PersonIcon from "@mui/icons-material/Person";
-import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import NightlightIcon from "@mui/icons-material/Nightlight";
 import InputAdornment from "@mui/material/InputAdornment";
@@ -16,6 +15,12 @@ import OutlinedInput from "@mui/material/OutlinedInput";
 import LoginIcon from "@mui/icons-material/Login";
 
 const Signup = () => {
+  const [transitionedIn, setTransitionedIn] = useState(false);
+
+  useEffect(() => {
+    setTransitionedIn(true);
+  }, []);
+
   const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
 
@@ -32,35 +37,35 @@ const Signup = () => {
     password: "",
   });
 
-  const [errors, setErrors] = useState([]);
+//   const [errors, setErrors] = useState([]);
   const { first_name, last_name, email, password } = formData;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const newUser = {
-      first_name,
-      last_name,
-      email,
-      password,
-    };
-    fetch("/signup", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newUser),
-    }).then((res) => {
-      if (res.ok) {
-        res.json().then((user) => {
-          navigate(`/`);
-          /*
-            Stretch goal: log in for the user automatically on sign up instead
-            of navigating to login page
-          */
-        });
-      } else {
-        res.json().then((json) => setErrors(Object.entries(json.errors)));
-      }
-    });
-  };
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     const newUser = {
+//       first_name,
+//       last_name,
+//       email,
+//       password,
+//     };
+//     fetch("/signup", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify(newUser),
+//     }).then((res) => {
+//       if (res.ok) {
+//         res.json().then((user) => {
+//           navigate(`/`);
+//           /*
+//             Stretch goal: log in for the user automatically on sign up instead
+//             of navigating to login page
+//           */
+//         });
+//       } else {
+//         res.json().then((json) => setErrors(Object.entries(json.errors)));
+//       }
+//     });
+//   };
 
   const changeHandler = (e) => {
     const { name, value } = e.target;
@@ -71,100 +76,111 @@ const Signup = () => {
     navigate("/");
   };
 
+//   if (errors) return <h1>{errors}</h1>;
   return (
     <div className="primary-container">
-      <div className="welcome-card">
-        <div className="light-dark-mode">
-          <NightlightIcon
-            sx={{
-              display: "flex",
-              fontSize: "74px",
-              color: "#1f1f1f",
-              cursor: "pointer",
-              transition: "color 0.3s",
-              "&:hover": {
-                color: "#e7d93c",
-              },
-            }}
-          />
-        </div>
-        <h1 style={{ fontSize: "88px" }}>Welcome</h1>
-        <p style={{ fontSize: "22px" }}>
-          nathanfoss.dev <b style={{ color: "#085786" }}>web</b>
-        </p>
-        <div className="flex flex-col md:flex-row">
-          <div className="flex flex-col items-center ">
-            <PersonIcon sx={{ color:"#c4c7c5", fontSize: "50px" }} />
-            <EmailIcon sx={{ color:"#c4c7c5", fontSize: "50px", marginTop:"200%" }} />
-            <LockIcon sx={{ color:"#c4c7c5", fontSize: "50px", marginTop:"40%" }} />
+      <div
+        className={`transition-in2 ${transitionedIn ? "transitioned-in2" : ""}`}
+      >
+        <div className="welcome-card">
+          <div className="light-dark-mode">
+            <NightlightIcon
+              sx={{
+                display: "flex",
+                fontSize: "74px",
+                color: "#1f1f1f",
+                cursor: "pointer",
+                transition: "color 0.3s",
+                "&:hover": {
+                  color: "#e7d93c",
+                },
+              }}
+            />
           </div>
-          <div className="w-full">
-            {/* Input fields column */}
-            <div className="flex flex-col space-y-4 items-center">
-              <TextField
-                fullWidth
-                label="First name"
-                variant="outlined"
-                name="first_name"
-                value={first_name}
-                onChange={changeHandler}
-                required
+          <h1 style={{ fontSize: "88px" }}>Welcome</h1>
+          <p style={{ fontSize: "22px" }}>
+            nathanfoss.dev <b style={{ color: "#085786" }}>web</b>
+          </p>
+          <div className="flex flex-col md:flex-row">
+            <div className="flex flex-col items-center ">
+              <PersonIcon sx={{ color: "#c4c7c5", fontSize: "50px" }} />
+              <EmailIcon
+                sx={{ color: "#c4c7c5", fontSize: "50px", marginTop: "200%" }}
               />
-              <TextField
-                fullWidth
-                label="Last name"
-                variant="outlined"
-                name="last_name"
-                value={last_name}
-                onChange={changeHandler}
-                required
+              <LockIcon
+                sx={{ color: "#c4c7c5", fontSize: "50px", marginTop: "40%" }}
               />
-              <TextField
-                fullWidth
-                label="Email"
-                variant="outlined"
-                name="email"
-                value={email}
-                onChange={changeHandler}
-                required
-              />
-              <FormControl fullWidth variant="outlined">
-                <InputLabel required htmlFor="outlined-adornment-password">
-                  Password
-                </InputLabel>
-                <OutlinedInput
-                  id="outlined-adornment-password"
-                  type={showPassword ? "text" : "password"}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        edge="end"
-                      >
-                        {showPassword ? (
-                          <VisibilityIcon />
-                        ) : (
-                          <VisibilityOffIcon />
-                        )}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                  label="Password"
-                  name="password"
-                  value={password}
+            </div>
+            <div className="w-full">
+              {/* Input fields column */}
+              <div className="flex flex-col space-y-4 items-center">
+                <TextField
+                  fullWidth
+                  label="First name"
+                  variant="outlined"
+                  name="first_name"
+                  value={first_name}
                   onChange={changeHandler}
+                  required
                 />
-              </FormControl>
+                <TextField
+                  fullWidth
+                  label="Last name"
+                  variant="outlined"
+                  name="last_name"
+                  value={last_name}
+                  onChange={changeHandler}
+                  required
+                />
+                <TextField
+                  fullWidth
+                  label="Email"
+                  variant="outlined"
+                  name="email"
+                  value={email}
+                  onChange={changeHandler}
+                  required
+                />
+                <FormControl fullWidth variant="outlined">
+                  <InputLabel required htmlFor="outlined-adornment-password">
+                    Password
+                  </InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? "text" : "password"}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showPassword ? (
+                            <VisibilityIcon />
+                          ) : (
+                            <VisibilityOffIcon />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Password"
+                    name="password"
+                    value={password}
+                    onChange={changeHandler}
+                  />
+                </FormControl>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="contact-card-wrapper" style={{ cursor: "pointer" }}>
-          <div className="contact-card" onClick={navigateToLoginPage}>
-            Back
+          <div className="contact-card-wrapper" style={{ cursor: "pointer" }}>
+            <div className="contact-card" onClick={navigateToLoginPage}>
+              Back
+            </div>
+            <div className="contact-card">
+              <LoginIcon /> Sign up
+            </div>
           </div>
-          <div className="contact-card"><LoginIcon /> Sign up</div>
         </div>
       </div>
     </div>
